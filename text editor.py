618471cec794f60efe013fd872d6ebbb56b9c9dd
Gouthambobby67 <97904722+Gouthambobby67@users.noreply.github.com>
2023-08-28 00:00:15 +0530

    Add files via upload

---
+++ text editor.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+    if file_path:
+        with open(file_path, "r") as file:
+            text.delete("1.0", tk.END)
+            text.insert("1.0", file.read())
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+    if file_path:
+        with open(file_path, "w") as file:
+            file.write(text.get("1.0", tk.END))
+
+root = tk.Tk()
+root.title("Simple Text Editor")
+
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+
+file_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+text = tk.Text(root)
+text.pack(fill=tk.BOTH, expand=True)
+
+root.mainloop()
